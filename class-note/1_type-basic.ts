20c4f0af6b9bf73fdecc73cf94570b1690980d1d
hsycamp <hsycamp@gmail.com>
2020-12-17 14:06:01 +0900

    변수와 함수 타입 정의하기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// JS 문자열 선언방식
+let strJs = "hello";
+
+// TS 문자열 선언방식
+let strTs: string = "hello";
+
+// TS 숫자 선언방식
+let num: number = 10;
+
+// TS 배열 선언방식
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hukl"];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플 선언방식
+let addtess: [string, number] = ["gangnam", 100];
+
+// TS 객체 선언방식
+let obj: object = {};
+// let person: object = {
+//   name: "capt",
+//   age: 100,
+// };
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+
+// TS 진위값 선언방식
+let show: boolean = true;
