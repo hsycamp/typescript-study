a8d55899f9a4972e2150b9d1be7c0221cd98358f
hsycamp <hsycamp@gmail.com>
2020-12-17 17:38:01 +0900

    Refactor : 오류 해결 및 구체적인 타입 정의

--- project/1_todo/src/index.ts
+++ project/1_todo/src/index.ts
@@ -1,46 +1,58 @@
-let todoItems;
+let todoItems: { id: number; title: string; done: boolean }[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
   const todos = [
-    { id: 1, title: "안녕", done: false },
-    { id: 2, title: "타입", done: false },
-    { id: 3, title: "스크립트", done: false },
+    { id: 1, title: "자료구조", done: false },
+    { id: 2, title: "알고리즘", done: false },
+    { id: 3, title: "네트워크", done: false },
   ];
   return todos;
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: { id: number; title: string; done: boolean }): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: { id: number; title: string; done: boolean }): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter((item) => item.done);
+function showCompleted(): object[] {
+  return todoItems.filter((item: { id: number; title: string; done: boolean }) => item.done);
 }
 
-function addTwoTodoItems() {}
+function addTwoTodoItems(): void {
+  const item4 = {
+    id: 4,
+    title: "운영체제",
+    done: false,
+  };
+  addTodo(item4);
+  addTodo({
+    id: 5,
+    title: "DB",
+    done: false,
+  });
+}
 
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
